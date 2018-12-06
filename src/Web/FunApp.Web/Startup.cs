b740bd7ec1c19469ba3806556513bc6eaa08de68
yotovds <yotovds@gmail.com>
2018-12-06 10:58:58 +0200

    Update Startup.cs

--- src/Web/FunApp.Web/Startup.cs
+++ src/Web/FunApp.Web/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using FunApp.Web.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using FunApp.Web.Models;
