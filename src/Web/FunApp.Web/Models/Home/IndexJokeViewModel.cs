7f328a9a884e1ebafafc02dd185993dcc7257dce
yotovds <yotovds@gmail.com>
2018-12-07 20:43:13 +0200

    Update IndexJokeViewModel.cs

--- src/Web/FunApp.Web/Models/Home/IndexJokeViewModel.cs
+++ src/Web/FunApp.Web/Models/Home/IndexJokeViewModel.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FunApp.Web.Models
+namespace FunApp.Web.Models.Home
 {
-    public class JokeViewModel
+    public class IndexJokeViewModel
     {
+        public string Content { get; set; }
+
+        public string CategoryName { get; set; }
     }
 }
