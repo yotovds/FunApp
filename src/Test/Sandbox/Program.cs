f050f01d89bb8257bc0bbd289d0003f333543ec2
yotovds <yotovds@gmail.com>
2018-12-07 14:28:17 +0200

    Create crawler for jokes

--- src/Test/Sandbox/Program.cs
+++ src/Test/Sandbox/Program.cs
@@ -1,9 +1,15 @@
-﻿using FunApp.Data;
+﻿using AngleSharp.Parser.Html;
+using FunApp.Data;
+using FunApp.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
 
 namespace Sandbox
 {
@@ -25,7 +31,61 @@ namespace Sandbox
 
         private static void SandboxCode(IServiceProvider serviceProvider)
         {
-            // TODO: Code here
+            var context = serviceProvider.GetService<FunAppContext>();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var parser = new HtmlParser();
+            var webClient = new WebClient { Encoding = Encoding.GetEncoding("windows-1251") };
+
+            for (var i = 4233; i < 10000; i++)
+            {
+                var url = "http://fun.dir.bg/vic_open.php?id=" + i;
+                string html = null;
+                for (int j = 0; j < 10; j++)
+                {
+                    try
+                    {
+                        html = webClient.DownloadString(url);
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        Thread.Sleep(10000);
+                    }
+                }
+
+                if (string.IsNullOrWhiteSpace(html))
+                {
+                    continue;
+                }
+
+                var document = parser.Parse(html);
+                var jokeContent = document.QuerySelector("#newsbody")?.TextContent?.Trim();
+                var categoryName = document.QuerySelector(".tag-links-left a")?.TextContent?.Trim();
+
+                if (!string.IsNullOrWhiteSpace(jokeContent) &&
+                    !string.IsNullOrWhiteSpace(categoryName))
+                {
+                    var category = context.Categories.FirstOrDefault(x => x.Name == categoryName);
+                    if (category == null)
+                    {
+                        category = new Category
+                        {
+                            Name = categoryName,
+                        };
+                    }
+
+                    var joke = new Joke()
+                    {
+                        Category = category,
+                        Content = jokeContent,
+                    };
+
+                    context.Jokes.Add(joke);
+                    context.SaveChanges();
+                }
+
+                Console.WriteLine($"{i} => {categoryName}");
+            }
         }
 
 
