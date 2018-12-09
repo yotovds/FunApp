5e9b9c5f08b0aa05494cd0241ff06942d83146e1
yotovds <yotovds@gmail.com>
2018-12-09 18:44:10 +0200

    Revert "Revert "Implement auto select in category dropdown menu""
    This reverts commit 96ed9c50e5fe04353b8dece3968880b5c567e036.

---
+++ src/Web/FunApp.Web/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using FunApp.Services.DataServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunApp.Web.Controllers
+{ 
+    public class CategoryController : BaseController
+    {
+        private readonly ICategoriesService categoriesService;
+        private readonly IJokeService jokesService;
+
+        public CategoryController(ICategoriesService categoriesService, IJokeService jokesService)
+        {
+            this.categoriesService = categoriesService;
+            this.jokesService = jokesService;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = this.categoriesService.GetAll().ToList();
+
+            return this.View(categories);
+        }
+
+        public IActionResult Details(int id, int? page)
+        {
+            //var jokesInCategory = this.jokesService.GetAllByCategory(id).ToList();
+
+            //var nextPage = page ?? 1;
+
+            //var pagedJokes = jokesInCategory.ToPagedList(nextPage, 4);
+
+            //return this.View(pagedJokes);
+
+            return this.View();
+        }
+    }
+}
