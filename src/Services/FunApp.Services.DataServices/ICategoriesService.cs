16e9cd9c2310b638150d047be7b8fa5a2dd97fe8
yotovds <yotovds@gmail.com>
2018-12-09 18:45:00 +0200

    Revert "Implement auto select in category dropdown menu"
    This reverts commit 288bc2bce5f101d740ed4bbfbf49eabd77f3c524.

--- src/Services/FunApp.Services.DataServices/ICategoriesService.cs
+++ src/Services/FunApp.Services.DataServices/ICategoriesService.cs
@@ -12,7 +12,5 @@ namespace FunApp.Services.DataServices
         IEnumerable<CategoryIdAndNameViewModel> GetAll();
 
         bool IsCategoryIdValid(int categoryId);
-
-        int? GetCategoryId(string name);
     }
 }
